3aea14fba1ffaa152c3ef8ed179c3ec03bd0524e
Ivan Hsiao <ivanhsiao125@gmail.com>
2026-08-25 15:51:06 +1000

    docs: add comments

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,6 +2,7 @@ import 'react-native-url-polyfill/auto';
 import * as SecureStore from 'expo-secure-store';
 import { createClient } from '@supabase/supabase-js';
 
+// Translate between SecureStore ops and Supabase client data
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
     return SecureStore.getItemAsync(key);
@@ -14,9 +15,11 @@ const ExpoSecureStoreAdapter = {
   },
 };
 
+// Read environment variables
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
+// Initialize Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: ExpoSecureStoreAdapter,
